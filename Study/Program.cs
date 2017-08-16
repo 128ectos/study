34e23f54eb7001a16469ef907a27de02330b59a0
128ectos <128ectos@gmail.com>
2017-08-15 22:47:20 -0700

    problem 1.2

--- Study/Program.cs
+++ Study/Program.cs
@@ -26,8 +26,22 @@ namespace Study
             log(string.Empty);
             log(string.Empty);
 
-
-
+            log("Problem 1.2");
+            new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("foo", "foo"),
+                new Tuple<string, string>("foo", "ofo"),
+                new Tuple<string, string>("foo", "oof"),
+                new Tuple<string, string>("bar", "arb"),
+                new Tuple<string, string>("foo", "bar"),
+                new Tuple<string, string>("foo", null),
+                new Tuple<string, string>(null, null),
+                new Tuple<string, string>(null, "baz"),
+                new Tuple<string, string>("foo", "baz"),
+                new Tuple<string, string>("fo", "baz")
+            }
+            .ToList()
+            .ForEach(x => log($"isPermutation(\"{x.Item1}\",\"{x.Item2}\") -> {IsPermutation(x.Item1, x.Item2)}"));
 
         }
 
@@ -52,8 +66,45 @@ namespace Study
             return true;
         }
 
+        // Problem 1.2
+        static bool IsPermutation(string a, string b)
+        {
+            // Null checking
+            if(a == null)
+            {
+                if(b == null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            if(b == null)
+            {
+                return false;
+            }
+
+            var charCounts = new Dictionary<char, int>();
+            foreach(var c in a)
+            {
+                if (!charCounts.ContainsKey(c))
+                {
+                    charCounts[c] = 0;
+                }
+                charCounts[c]++;
+            }
+
+            foreach(var c in b)
+            {
+                if(!charCounts.ContainsKey(c))
+                {
+                    return false;
+                }
+                charCounts[c]--;
+            }
+
+            return charCounts.Values.All(x => x == 0);
+        }
 
         #endregion
-
     }
 }
