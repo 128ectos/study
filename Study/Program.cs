1d47d18a2216d4a2e30d77483e7abfdfab6a047a
128ectos <128ectos@gmail.com>
2017-08-15 22:00:11 -0700

    Adding first problem

--- Study/Program.cs
+++ Study/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Study
 {
@@ -6,8 +8,52 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("foo");
+            Action<string> log = Console.WriteLine;
+
+            log("Problem 1.1");
+            new[]
+            {
+                "foo",
+                "bar",
+                "baz",
+                "rrr",
+                "",
+                null
+            }
+            .ToList()
+            .ForEach(x => log($"isUnique(\"{x}\") -> {IsUnique(x)}"));
+
+            log(string.Empty);
+            log(string.Empty);
+
+
+
 
         }
+
+        // Start off just inlining problems in this file to get bootstrapped
+        // Once enough problems done will have to refactor organization
+
+        #region Chapter 1
+
+        // Problem 1.1
+        static bool IsUnique(string str)
+        {
+            if(str == null) { return true; } // Depends on requirement
+            var seen = new HashSet<char>();
+            foreach(var c in str)
+            {
+                if(seen.Contains(c))
+                {
+                    return false;
+                }
+                seen.Add(c);
+            }
+            return true;
+        }
+
+
+        #endregion
+
     }
 }
